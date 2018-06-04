b6e5af5733aa96605d80f0552c77d16cd8cf8667
Ismael Perez <ismaelperezemail@gmail.com>
2018-06-03 20:18:02 -0700

    Integrations test: Moxios used to intecept axios call

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -25,12 +25,19 @@ class CommentBox extends Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <h4>Add a Comment</h4>
-                    <textarea onChange={this.handleChange} value={this.state.comment}/>
+                    <textarea
+                        onChange={this.handleChange}
+                        value={this.state.comment}
+                    />
                     <div>
                         <button>Submit Comment</button>
                     </div>
                 </form>
-                <button onClick={this.props.fetchComments}>Fetch Comments</button>
+                <button
+                    className='fetch-comments'
+                    onClick={this.props.fetchComments}>
+                    Fetch Comments
+                </button>
             </div>
         )
     }
